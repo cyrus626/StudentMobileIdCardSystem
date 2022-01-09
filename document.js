93adca12909059326b6ee8e755a37b43637da6b1
cyrus626 <momohcyrus@gmail.com>
2022-01-09 13:30:41 +0100

    some important update

--- document.js
+++ document.js
@@ -1,6 +1,6 @@
 function myFunctn(){
-    var pword0 = document.getElementById("pword0");
-    var pword1 = document.getElementById("pword1");
+    var pword0 = document.getElementById("pword0").value;
+    var pword1 = document.getElementById("pword1").value;
 
     if(pword0 != pword1){
         document.getElementById("reslt").innerHTML = "Password not matched";
